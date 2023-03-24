dbf0df466ca69f82c145c698490b183e860f29b8
Pal Nguyen <pnpal2002@gmail.com>
2023-03-24 10:41:50 +0700

    init

---
+++ app/src/main/java/com/example/csc13009_android_ckdp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.csc13009_android_ckdp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
