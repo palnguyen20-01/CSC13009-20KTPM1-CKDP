88a914592ad0989051c02aa5b7325ae46123dda1
Phúc Nguyễn <phuc4570@gmail.com>
2023-04-03 22:44:25 +0700

    feat: Search in GG Map; Tracking Postion; Only Show Hospital

--- app/src/main/java/com/example/csc13009_android_ckdp/HospitalMap/HospitalMapActivity.kt
+++ app/src/main/java/com/example/csc13009_android_ckdp/HospitalMap/HospitalMapActivity.kt
@@ -1,13 +1,21 @@
 package com.example.csc13009_android_ckdp.HospitalMap
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,14 +25,18 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.csc13009_android_ckdp.R
 import com.example.csc13009_android_ckdp.databinding.ActivityHospitalMapBinding
+import com.google.android.gms.maps.model.MapStyleOptions
 import java.util.*
+import kotlin.collections.ArrayList
 
 class HospitalMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityHospitalMapBinding
+    private lateinit var mapSearchView : androidx.appcompat.widget.SearchView
     private val REQUEST_LOCATION_PERMISSION = 1
-
+    private val TAG = HospitalMapActivity::class.java.simpleName
+    private var oldPostion : LatLng = LatLng(0.0,0.0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,17 +47,37 @@ class HospitalMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mapSearchView = findViewById(R.id.mapSearchView)
+        mapSearchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val location = mapSearchView.query.toString()
+                var addressList : MutableList<Address?>? = mutableListOf()
+
+                if(location != null || location == ""){
+                    val geocoder : Geocoder = Geocoder(this@HospitalMapActivity)
+                    try {
+                        addressList = geocoder.getFromLocationName(location,10000)
+
+                    }catch (ex : java.lang.Exception){
+                        ex.printStackTrace()
+                    }
+
+                    for(i in 0..addressList!!.size-1){
+                        val address = addressList[i]
+                        val latLng = LatLng(address!!.latitude,address!!.longitude)
+                        map.addMarker(MarkerOptions().position(latLng).title(location))
+                    }
+                }
+
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.map_options, menu)
@@ -56,6 +88,7 @@ class HospitalMapActivity : AppCompatActivity(), OnMapReadyCallback {
         // Change the map type based on the user's selection.
         R.id.normal_map -> {
             map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            var mapStyleOptions : MapStyleOptions
             true
         }
         R.id.hybrid_map -> {
@@ -75,16 +108,36 @@ class HospitalMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        val latitude = 10.762835
-        val longitude = 106.682482
+        /*val latitude = map.myLocation.latitude
+        val longitude = map.myLocation.longitude
         val homeLatLng = LatLng(latitude, longitude)
-        var zoomLevel = 20f
+        var zoomLevel = 20f*/
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-        map.addMarker(MarkerOptions().position(homeLatLng))
+        /*map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        map.addMarker(MarkerOptions().position(homeLatLng))*/
+        setMapStyle(map)
         setMapLongClick(map)
         setPoiClick(map)
+        setPositionChange(map)
         enableMyLocation()
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this,
+                    R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        }catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
     }
 
     private fun setMapLongClick(map: GoogleMap) {
@@ -132,6 +185,21 @@ class HospitalMapActivity : AppCompatActivity(), OnMapReadyCallback {
                 arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                 REQUEST_LOCATION_PERMISSION
             )
+        }
+    }
+
+    private fun setPositionChange(map : GoogleMap){
+        map.setOnMyLocationChangeListener {location ->
+            val myLocation = LatLng(location.latitude,location.longitude)
+            val zoomLevel = 20f
+            if((oldPostion.latitude == 0.0 && oldPostion.longitude == 0.0) ||
+                    myLocation.longitude - oldPostion.longitude > 0.005 ||
+                    myLocation.latitude - oldPostion.latitude > 0.005) {
+                map.clear()
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, zoomLevel))
+                map.addMarker(MarkerOptions().position(myLocation))
+                oldPostion = myLocation
+            }
         }
     }
 
