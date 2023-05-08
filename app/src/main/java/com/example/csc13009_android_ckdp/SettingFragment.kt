e42bed8d0ae7915ee29a3b24dfa57d9bd294a28b
CHIẾN HUỲNH MINH <huynhchien293@gmail.com>
2023-05-08 22:16:10 +0700

    update: About us.

--- app/src/main/java/com/example/csc13009_android_ckdp/SettingFragment.kt
+++ app/src/main/java/com/example/csc13009_android_ckdp/SettingFragment.kt
@@ -1,12 +1,12 @@
 package com.example.csc13009_android_ckdp
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.text.Html
-import android.text.method.LinkMovementMethod
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +16,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.csc13009_android_ckdp.Profile.EmailChange
 import com.example.csc13009_android_ckdp.Profile.InfoChange
@@ -24,6 +26,7 @@ import com.example.csc13009_android_ckdp.utilities.RequestCodeResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import java.util.*
+
 
 class SettingFragment : Fragment() {
 
@@ -36,6 +39,10 @@ class SettingFragment : Fragment() {
     lateinit var aboutUsText: TextView
     lateinit var txtPrivacyItem1: TextView
     lateinit var privacyText: TextView
+    lateinit var phoneAboutUsText: TextView
+    lateinit var emailAboutUsText: TextView
+    lateinit var fbImg: ImageView
+    lateinit var ytbImg: ImageView
     lateinit var cardPass : CardView
     lateinit var cardInfo : CardView
     lateinit var cardEmail : CardView
@@ -132,11 +139,15 @@ class SettingFragment : Fragment() {
         aboutUsText = settingsFragment.findViewById<TextView>(R.id.aboutusTextProfile)
         privacyText = settingsFragment.findViewById<TextView>(R.id.privacyTextProfile)
         txtPrivacyItem1 = settingsFragment.findViewById<TextView>(R.id.txtPrivacyItem1)
+        phoneAboutUsText = settingsFragment.findViewById<TextView>(R.id.phone_call)
+        emailAboutUsText = settingsFragment.findViewById<TextView>(R.id.email_aboutus)
         //cardPhone = settingsFragment.findViewById<CardView>(R.id.card_phone)
         cardLogout = settingsFragment.findViewById<CardView>(R.id.card_logout)
         txtProfileEmail = settingsFragment.findViewById<TextView>(R.id.txtProfileEmail)
         txtProfileName = settingsFragment.findViewById<TextView>(R.id.txtProfileName)
         imageProfile = settingsFragment.findViewById<ImageView>(R.id.imageProfile)
+        fbImg = settingsFragment.findViewById<ImageView>(R.id.facebook)
+        ytbImg = settingsFragment.findViewById<ImageView>(R.id.youtube)
         profileScrollView = settingsFragment.findViewById<ScrollView>(R.id.profileScrollView)
         privacyScrollView = settingsFragment.findViewById<ScrollView>(R.id.privacyScrollView)
         aboutUsScrollView = settingsFragment.findViewById<ScrollView>(R.id.aboutUsScrollView)
@@ -181,9 +192,72 @@ class SettingFragment : Fragment() {
             startActivity(intent)
 
         }
+        phoneAboutUsText.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_CALL)
+            mIntent.data = Uri.parse("tel:+84 903612640")
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    CALL_PHONE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    main!!, arrayOf(CALL_PHONE),
+                    RequestCodeResult.MY_PERMISSIONS_REQUEST_CALL_PHONE
+                )
+
+            } else {
+                try {
+                    startActivity(mIntent)
+                } catch (e: SecurityException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        emailAboutUsText.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            val uriText = "mailto:" + Uri.encode("20127007@student.hcmus.edu.vn") + "?subject=" +
+                    Uri.encode("your email id ") + "&body=" + Uri.encode("")
+
+            val uri = Uri.parse(uriText)
+            intent.data = uri
+            startActivity(Intent.createChooser(intent, "Send Email"))
+        }
+        fbImg.setOnClickListener {
+            var uri = Uri.parse("https://www.facebook.com/z.phuc4570");
+            startActivity(Intent(Intent.ACTION_VIEW,uri))
+        }
+        ytbImg.setOnClickListener {
+            var uri = Uri.parse("https://www.youtube.com/@PhucNguyen-jc3iw");
+            startActivity(Intent(Intent.ACTION_VIEW,uri))
+        }
         return settingsFragment
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            RequestCodeResult.MY_PERMISSIONS_REQUEST_CALL_PHONE -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0
+                    && grantResults[0] === PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    // permission was granted, yay! Do the phone call
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+        }
+    }
     private fun showToast(message: String){
         Toast.makeText(main, message, Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/example/csc13009_android_ckdp/utilities/RequestCodeResult.kt
+++ app/src/main/java/com/example/csc13009_android_ckdp/utilities/RequestCodeResult.kt
@@ -8,5 +8,6 @@ public class RequestCodeResult {
         const val CHANGE_PASSWORD: Int = 5200
         const val CHANGE_INFORMATION: Int = 5201
         const val CHANGE_EMAIL: Int = 5202
+        const val MY_PERMISSIONS_REQUEST_CALL_PHONE: Int = 5230
     }
 }
